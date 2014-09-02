ed14fa1732ea734b1edfda4e41904177a1b56d16
yakisuzu <yakisuzu>
2014-09-02 23:32:30 +0900

    add junit4, add test class, create login sample

--- src/main/java/features/WwwGoogleCoJpFeature.java
+++ src/main/java/features/WwwGoogleCoJpFeature.java
@@ -1,15 +1,20 @@
 package features;
 
 import operators.wwwgooglecojp.WwwGoogleCoJpOperator;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import pages.wwwgooglecojp.WwwGoogleCoJpPage;
+import support.PageProperties;
 
 public class WwwGoogleCoJpFeature {
-	public static void main(String[] args) {
-		WwwGoogleCoJpPage page =  new WwwGoogleCoJpPage(new FirefoxDriver());
-		WwwGoogleCoJpOperator ope = new WwwGoogleCoJpOperator(page);
-		ope.clickGmail();
+	public void login() {
+		WebDriver driver = new FirefoxDriver();
+		WwwGoogleCoJpOperator ope = new WwwGoogleCoJpOperator(new WwwGoogleCoJpPage(driver));
 
-		page.quit();
+		String email = PageProperties.getInstance().getProperty("wwwgooglecojp.loginId");
+		String pass = PageProperties.getInstance().getProperty("wwwgooglecojp.loginPass");
+		ope.enterLogin(email, pass);
+
+		ope.quit();
 	}
 }
---
+++ src/test/java/features/WwwGoogleCoJpFeatureTest.java
@@ -0,0 +1,10 @@
+package features;
+
+import org.junit.Test;
+
+public class WwwGoogleCoJpFeatureTest {
+	@Test
+	public void testLogin() throws Exception {
+		new WwwGoogleCoJpFeature().login();
+	}
+}
