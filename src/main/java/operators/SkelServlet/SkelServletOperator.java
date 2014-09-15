aa3f41072c36a9390d21100b5759fd2bd01340f1
yakisuzu <yakisuzu@gmail.com>
2014-09-15 15:25:28 +0900

    add skelservlet page

---
+++ src/main/java/operators/SkelServlet/SkelServletOperator.java
@@ -0,0 +1,22 @@
+package operators.SkelServlet;
+
+import operators.Operator;
+import org.openqa.selenium.WebDriver;
+import pages.colleague.SkelServlet.SkelServletColleague;
+import pages.mediator.SkelServlet.SkelServletMediator;
+
+public class SkelServletOperator extends Operator<SkelServletMediator> {
+	public SkelServletOperator(WebDriver driver) {
+		super(driver);
+	}
+
+	private SkelServletColleague coll1 = getColleague(SkelServletColleague.class);
+
+	public void clickWinOpen() {
+		execute(new Runnable() {
+			@Override
+			public void run() {
+			}
+		});
+	}
+}
